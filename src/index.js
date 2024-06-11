a7914b6b04b033e2b79ccde804a4fe73bd9691e5
Emir <ebasic@gmail.com>
2024-06-11 23:19:55 +0200

    L1.10 Exercise: Form Validation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ const app = Vue.createApp({
     <button v-on:click="increment"> Increment </button>
     <p>{{ count }}</p>
 
+    <input
+      v-bind:value="value"
+      v-on:input="input"
+    >
+    <div class="red">
+      {{ error }}
+    </div>
+
     <div v-for="number in numbers"
          v-bind:class="getClass(number)"
      >
@@ -17,15 +25,24 @@ const app = Vue.createApp({
   data() {
     return {
       count: 0,
+      value: 'user',
       numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
   },
   computed: {
     evenList() {
       return this.numbers.filter((num) => this.isEven(num))
+    },
+    error() {
+      if (this.value.length < 5) {
+        return 'Must be greater than 5'
+      }
     }
   },
   methods: {
+    input($event) {
+      this.value = $event.target.value
+    },
     getClass(number) {
       return this.isEven(number) ? 'blue' : 'red'
     },
